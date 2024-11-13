56227c20fb992b3437610d680bb6a75afdb36010
siddhu-reddy <121557022+siddhu-reddy@users.noreply.github.com>
2024-11-13 17:19:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-
+import csv
 url = "https://dealsheaven.in/"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
-deal_cards = soup.find_all('div', class_='deatls-inner') 
-for deal_card in deal_cards:
-    title = deal_card.find('h3').text.strip()
-    price = deal_card.find('p', class_='price').text.strip()
-    special_price = deal_card.find('p', class_='spacail-price').text.strip()
-    print(f"Title: {title}")
-    print(f"Original Price: {price}")
-    print(f"Special Price: {special_price}")
-    print()
+with open('deals.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Title', 'Original Price', 'Special Price'])
 
+    deal_cards = soup.find_all('div', class_='deatls-inner') 
+    for deal_card in deal_cards:
+        title = deal_card.find('h3').text.strip()
+        price = deal_card.find('p', class_='price').text.strip()
+        special_price = deal_card.find('p', class_='spacail-price').text.strip()
+        
+        writer.writerow([title, price, special_price])
+print("Data saved to deals.csv")
